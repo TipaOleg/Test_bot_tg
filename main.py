11f6a4f064481e0b8602c45f5e29e263d360bc9e
TipaOleg <iroh2021@gmail.com>
2025-01-15 06:42:02 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
+
+with open('Key', 'r') as file:
+    API = file.read().strip()
+
+bot = Bot(token=API)
+dp = Dispatcher(storage=MemoryStorage())
+
+@dp.message(Command(commands=['start']))
+async def start(message: Message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message()
+async def all_message(message: Message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
