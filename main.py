71e0a7e7ab6b4f5f0b66208bac715def8a9c2279
TipaOleg <iroh2021@gmail.com>
2025-01-26 16:43:40 +0300

    five commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, \
+    CallbackQuery
 from aiogram.filters import Command
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.filters.callback_data import CallbackData
 import asyncio
 
 
@@ -12,12 +14,18 @@ with open('Key', 'r') as file:
 
 bot = Bot(token=API)
 dp = Dispatcher(storage=MemoryStorage())
-keyboard = ReplyKeyboardMarkup(
+main_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='Рассчитать')],
         [KeyboardButton(text='Информация')]
     ],
     resize_keyboard=True
+)
+inline_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')],
+        [InlineKeyboardButton(text="Формулы расчета", callback_data='formulas')]
+    ]
 )
 
 class UserState(StatesGroup):
@@ -27,17 +35,26 @@ class UserState(StatesGroup):
 
 @dp.message(Command(commands=['start']))
 async def start(message: Message):
-    await message.answer('Привет! Я бот помогающий твоему здоровью. Выберите действие:', reply_markup=keyboard)
+    await message.answer('Привет! Я бот помогающий твоему здоровью. Выберите действие:', reply_markup=main_keyboard)
 
 @dp.message(lambda message: message.text == 'Рассчитать')
-async def set_age(message: Message, state: FSMContext):
-    await message.answer('Ввудите свой возраст:')
-    await state.set_state(UserState.age)
+async def main_menu(message: Message):
+    await message.answer('Выберите опцию', reply_markup=inline_keyboard)
 
-# @dp.message(Command(commands=['Calories']))
-# async def set_age(message: Message, state: FSMContext):
-#     await message.answer('Ввудите свой возраст:')
-#     await state.set_state(UserState.age)
+@dp.message(lambda message: message.text == 'Информация')
+async def info(message: Message):
+    await message.answer('Это бот для улучшения вашего здоровья.')
+
+@dp.callback_query(lambda call: call.data == 'formulas')
+async def get_formulas(call: CallbackQuery):
+    await call.message.answer('Формула Миффлина-Сан Жеора: 10*вес + 6.25*рост - 5*возраст + 5')
+    await call.answer()
+
+@dp.callback_query(lambda call: call.data == 'calories')
+async def set_age(call: CallbackQuery, state: FSMContext):
+    await call.message.answer('Введите свой возраст')
+    await state.set_state(UserState.age)
+    await call.answer()
 
 @dp.message(UserState.age)
 async def set_growth(message: Message, state: FSMContext):
